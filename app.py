b4314a28991e0f680be215444af82449583852c7
kali <kali@kali.kali>
2026-04-19 08:56:19 -0500

    fix: proper Flask app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-return "hi"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "DevSecOps project is running!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
